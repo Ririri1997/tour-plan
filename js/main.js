eebdc94c851e38b5a56f8fb9918f817f8c342279
Ririri1997 <ircher1997@gmail.com>
2023-01-24 17:36:14 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    prevEl: '.slider-button--prev',
+    nextEl: '.slider-button--next',
+  },
+});
