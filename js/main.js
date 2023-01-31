75396769d2150b3c96b178755040a2bd5b750dfb
Ririri1997 <ircher1997@gmail.com>
2023-01-31 16:55:42 +0300

    create: reviews setion and fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    prevEl: '.slider-button--prev',
-    nextEl: '.slider-button--next',
+    prevEl: '.hotel-slider__button--prev',
+    nextEl: '.hotel-slider__button--next',
+  },
+});
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    prevEl: '.reviews-slider__button--prev',
+    nextEl: '.reviews-slider__button--next',
   },
 });
